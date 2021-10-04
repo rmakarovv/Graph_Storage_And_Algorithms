2a78961ec5e2164fcaf96072018ff1b3bcaebce7
rmakarovv <rscm544@gmail.com>
2021-10-04 09:51:35 +0300

    Update Graph_Algorithms.cpp

--- Graph_Algorithms.cpp
+++ Graph_Algorithms.cpp
@@ -15,8 +15,6 @@
 #include <algorithm>
 
 #define INF 10000007 // This constant is used for unknown path in Dijkstra algorithm
-using namespace std;
-
 
 /**
  * The class that represents a vertex in the graph.
@@ -25,7 +23,7 @@ template <typename T>
 class Vertex {
 public:
     T name;
-    string marked = "WHITE";
+    std::string marked = "WHITE";
 
     Vertex() {};
 
@@ -37,11 +35,11 @@ public:
         name = vertex.name;
     }
 
-    Vertex(T s){
+    Vertex(T s) {
         name = s;
     }
 
-    friend bool operator==(const Vertex& left, const Vertex& right){
+    friend bool operator == (const Vertex& left, const Vertex& right) {
         return left.name == right.name;
     }
 };
@@ -51,7 +49,7 @@ public:
  */
 template <typename T>
 bool operator < (const Vertex<T> &v, const Vertex<T> &v2) {
-    return ( v2.name < v.name );
+    return (v2.name < v.name);
 }
 template <typename T>
 bool operator == (Vertex<T> &rhs, Vertex<T> &lhs) {
@@ -93,7 +91,7 @@ public:
         bandwidth = edge.bandwidth;
     }
 
-    friend bool operator==(const Edge& left, const Edge& right) {
+    friend bool operator == (const Edge& left, const Edge& right) {
         return (left.from == right.from && left.to == right.to && left.weight == right.weight);
     }
 };
@@ -106,32 +104,26 @@ public:
 template<typename T>
 class Graph {
 public:
-    list<Vertex<T>> ver;
-    list<Edge<T>> allEdges;
+    std::map<T, Vertex<T>> vertices;
+    std::map<std::pair<T, T>, Edge<T>> edges;
 
     Graph(){};
 
     virtual Vertex<T>& addVertex(T name) = 0;
 
-    virtual void removeVertex(T name){};
+    virtual void removeVertex(T name) = 0;
 
     virtual Edge<T>& addEdge(int w, T f, T t) = 0;
 
     virtual Edge<T>& addEdgeBandwidth(int w, T f, T t, int b) = 0;
 
-    virtual void removeEdge(T from, T to){};
+    virtual void removeEdge(T from, T to) = 0;
 
-    virtual vector<T> edgesFrom(T name){vector<T> v; return v;};
+    virtual std::vector<T> edgesFrom(T name) = 0;
 
-    virtual vector<Edge<T>> edgesTo(T name){vector<Edge<T>> v; return v;};
+    virtual std::vector<Edge<T>> edgesTo(T name) = 0;
 
-    virtual Vertex<T> findVertex(T name){return Vertex<T>();};
-
-    virtual Edge<T> findEdge(T from, T to){return Edge<T>();}
-
-    virtual bool vertexExist(T name){return true;};
-
-    virtual bool hasEdge(T from, T to){return true;};
+    virtual bool hasEdge(T from, T to) = 0;
 };
 
 
@@ -145,8 +137,8 @@ public:
     // The representation of the adjacency matrix graph.
     // The names of the vertices are mapped to the information about them.
     // A pair of vertices is mapped to an edge between them if such edge exist.
-    map<T, Vertex<T>> vertices;
-    map<pair<T, T>, Edge<T>> edges;
+    std::map<T, Vertex<T>> vertices;
+    std::map<std::pair<T, T>, Edge<T>> edges;
 
     // Default constructor.
     AdjacencyMatrixGraph(){};
@@ -158,20 +150,24 @@ public:
             Vertex<T> vertex(name);
             vertices[name] = vertex;
         }
+
         return vertices[name];
     }
 
     // The function that allows removing a vertex.
     // It also removes all edges that are connected to this vertex.
     void removeVertex(T name) override {
-        vector<pair<T, T>> toDelete;
+        std::vector<std::pair<T, T>> toDelete;
+
         for (const auto &el : edges) {
             if (el.first.first == name || el.first.second == name) {
                 toDelete.push_back({el.first.first, el.first.second});
             }
         }
+
         vertices.erase(name);
-        for (pair<T, T> &toDel : toDelete) {
+
+        for (std::pair<T, T> &toDel : toDelete) {
             edges.erase({toDel.first, toDel.second});
         }
     }
@@ -192,12 +188,14 @@ public:
     // The functions that allows removing edges that have a bandwidth
     // less than some specified value.
     void removeEdgeBandwidth(int b) {
-        vector< pair<int,int> > toRemove;
+        std::vector< std::pair<int, int> > toRemove;
+
         for (const auto &el : edges) {
             if (el.second.bandwidth < b)
                 toRemove.push_back({el.first.first, el.first.second});
         }
-        for (pair<int, int> &el : toRemove) {
+
+        for (std::pair<int, int> &el : toRemove) {
             edges.erase(el);
         }
     }
@@ -210,25 +208,29 @@ public:
 
     // The functions that returns all names of
     // the vertices that are adjacent to some vertex.
-    vector<T> edgesFrom(T name) override {
-        vector<T> ans;
+    std::vector<T> edgesFrom(T name) override {
+        std::vector<T> ans;
+
         for (auto &i : edges) {
             if (i.first.first == name) {
                 ans.push_back(i.first.second);
             }
         }
+
         return ans;
     }
 
     // The functions that returns all names of
     // the vertices that have a path to some vertex.
-    vector<Edge<T>> edgesTo(T name) override {
-        vector<Edge<T>> ans;
+    std::vector<Edge<T>> edgesTo(T name) override {
+        std::vector<Edge<T>> ans;
+
         for (auto &i : edges) {
             if (i.first.second == name) {
                 ans.push_back(i.second);
             }
         }
+
         return ans;
     }
 
@@ -240,7 +242,8 @@ public:
 
     // The function that transposes a graph.
     void transpose() {
-        vector<pair<pair<T, T>, Edge<T>>> edgesToReverse;
+        std::vector< std::pair< std::pair<T, T>, Edge<T> > > edgesToReverse;
+
         for (const auto &el : edges) {
             edgesToReverse.push_back({{el.first.first, el.first.second}, el.second});
         }
@@ -263,13 +266,14 @@ public:
      * If the vertex is marked "GREY", then it is being processed now.
      * If the vertex is marked "BLACK" then is has already been processed.
      */
-    bool DFS(Vertex<T> &start, stack<Vertex<T>> &processed) {
+    bool DFS(Vertex<T> &start, std::stack<Vertex<T>> &processed) {
         processed.push(start);
         // If the vertex was already processed, then we are in the cycle
         if (start.marked == "GREY") {
             return false;
         }
-        vector<T> adjacent = edgesFrom(start.name);
+
+        std::vector<T> adjacent = edgesFrom(start.name);
 
         start.marked = "GREY";
 
@@ -285,7 +289,7 @@ public:
         // Recursive DFS with the check for a return value.
         for (T &ver : adjacent) {
             bool ans = DFS(vertices[ver], processed);
-            if ( !ans ) {
+            if (!ans) {
                 return false;
             }
         }
@@ -304,30 +308,35 @@ public:
      * If some cycle is found the function prints its weight and involved vertices.
      */
     void isAcyclic() {
-        stack<Vertex<T>> processed;
+        std::stack<Vertex<T>> processed;
         bool isDFSPossible = true; // Detect if there is DFS possible a cycle
+
         // DFS is performed on each vertex to detect a cycle if it is present.
         for (auto &el : vertices) {
             // At each iteration vertices are marked "WHITE", and stack getting clear.
             for (auto &ver : vertices) {
                 ver.second.marked = "WHITE";
             }
+
             while (!processed.empty())
                 processed.pop();
 
             // DFS for each vertex.
             isDFSPossible = DFS(el.second, processed);
+
             // If cycle is found then the loop breaks out to print it.
             if (!isDFSPossible)
                 break;
         }
 
         if (isDFSPossible) {
-            cout << "ACYCLIC" << endl;
+            std::cout << "ACYCLIC" << '\n';
         } else {
             // Constructing a path of the loop
             Vertex<T> looped = processed.top(); // The vertex in which loop was detected.
-            vector<Vertex<T>> path;
+
+            std::vector<Vertex<T>> path;
+
             // Pushing looped vertex into the path.
             path.push_back(processed.top());
             processed.pop();
@@ -337,25 +346,27 @@ public:
                 path.push_back(processed.top());
                 processed.pop();
             }
+
             path.push_back(path.front());
+
             // Path is reversed because previously is was in the stack
-            reverse(path.begin(), path.end());
+            std::reverse(path.begin(), path.end());
 
             // Weight is calculated by simply adding length of the edges.
             long long weight = 0;
             int i = 0;
-            while ((i+1) != path.size()) {
-                weight += edges[{path[i].name, path[i+1].name}].weight;
+            while (i + 1 != path.size()) {
+                weight += edges[{path[i].name, path[i + 1].name}].weight;
                 i++;
             }
             path.pop_back();
 
             // Printing weight of the cycle and vertices inside it.
-            cout << weight << " ";
+            std::cout << weight << " ";
             for (const Vertex<T>& ver : path) {
-                cout << ver.name << " ";
+                std::cout << ver.name << " ";
             }
-            cout << endl;
+            std::cout << '\n';
         }
     }
 };
@@ -369,32 +380,32 @@ public:
  * 4) Transpose a graph
  * 5) Check if the graph is acyclic.
  */
-void general(){
+void general() {
     // Variables for command, names and weight.
-    string command, name1, name2;
+    std::string command, name1, name2;
     int weight;
 
     // Initializing graph.
-    AdjacencyMatrixGraph<string> graph;
+    AdjacencyMatrixGraph<std::string> graph;
 
     // Reading until the end of the file and performing commands
-    while (cin >> command) {
-        if (command == "ADD_VERTEX"){
-            cin >> name1;
+    while (std::cin >> command) {
+        if (command == "ADD_VERTEX") {
+            std::cin >> name1;
             graph.addVertex(name1);
         } else if (command == "ADD_EDGE") {
-            cin >> name1 >> name2 >> weight;
+            std::cin >> name1 >> name2 >> weight;
             graph.addEdge(weight, name1, name2);
         } else if (command == "HAS_EDGE") {
-            cin >> name1 >> name2;
+            std::cin >> name1 >> name2;
             if (graph.hasEdge(name1, name2))
-                cout << "TRUE" << endl;
-            else cout << "FALSE" << endl;
+                std::cout << "TRUE" << '\n';
+            else std::cout << "FALSE" << '\n';
         } else if (command == "REMOVE_EDGE") {
-            cin >> name1 >> name2;
+            std::cin >> name1 >> name2;
             graph.removeEdge(name1, name2);
         } else if (command == "REMOVE_VERTEX") {
-            cin >> name1;
+            std::cin >> name1;
             graph.removeVertex(name1);
         } else if (command == "IS_ACYCLIC") {
             graph.isAcyclic();
@@ -410,9 +421,9 @@ void general(){
  * and previousVertex(from which current was accessed).
  * The last parameter by which pairs are compared is length of the edge.
  */
-class Compare{
+class Compare {
 public:
-    bool operator() (pair< pair<int, int>,int> p1, pair< pair<int, int>,int> p2){
+    bool operator() (std::pair< std::pair<int, int>, int > p1, std::pair< std::pair<int, int>, int > p2){
         return (p1.second > p2.second);
     }
 };
@@ -427,36 +438,38 @@ public:
 void shortestPath() {
     // Initializing number of vertices, number of queueOfVertices and graph.
     int n, m;
-    cin >> n >> m;
+    std::cin >> n >> m;
+
     int source, target, length, bandwidth;
     AdjacencyMatrixGraph<int> graph;
 
     // Filling the graph with vertices
-    for (int i = 1; i < n+1; i++) {
+    for (int i = 1; i < n + 1; i++) {
         graph.addVertex(i);
     }
 
     // Filling the graph with queueOfVertices
     for (int i = 0; i < m; i++) {
-        cin >> source >> target >> length >> bandwidth;
+        std::cin >> source >> target >> length >> bandwidth;
         graph.addEdgeBandwidth(length, source, target, bandwidth);
     }
 
     // Initializing start and finish vertex, and minimal bandwidth.
     int start, finish, minBandwidth;
-    cin >> start >> finish >> minBandwidth;
+    std::cin >> start >> finish >> minBandwidth;
 
     // Removing all queueOfVertices that has bandwidth less than required
     graph.removeEdgeBandwidth(minBandwidth);
 
     // Initializing priority queue for vertices
-    priority_queue<pair<pair<int, int>, int>, vector<pair<pair<int, int>, int>>, Compare > queueOfVertices;
+    std::priority_queue<std::pair< std::pair<int, int>, int>,
+                    std::vector< std::pair< std::pair<int, int>, int > >, Compare > queueOfVertices;
 
     // The algorithm is build for vertices that have 'int' name.
     int current = start, previous = current; // current and previous vertices are assigned
-    map<int, vector<int>> shortestPaths; // shortest path between source and each vertex
-    map<int, int> shortestLength; // Shortest length between source and each vertex
-    vector<int> path; // For creating a shortest path.
+    std::map<int, std::vector<int>> shortestPaths; // shortest path between source and each vertex
+    std::map<int, int> shortestLength; // Shortest length between source and each vertex
+    std::vector<int> path; // For creating a shortest path.
 
     // Initializing variables for starting vertex
     path.push_back(start);
@@ -464,11 +477,11 @@ void shortestPath() {
     shortestLength[start] = 0;
 
     // To know if the vertex was already added to set
-    vector<bool> marked(n+3, false);
+    std::vector<bool> marked(n + 3, false);
     marked[start] = true;
 
     // All adjacent vertices of starting are pushed to the queue
-    vector<int> adjacent = graph.edgesFrom(start);
+    std::vector<int> adjacent = graph.edgesFrom(start);
     for (const int &el : adjacent) {
         queueOfVertices.push({{el, start}, graph.edges[{start, el}].weight});
     }
@@ -512,25 +525,36 @@ void shortestPath() {
         path = shortestPaths[finish]; // Path is stored in the created map.
         int i = 0;
         // Calculating total bandwidth
-        while ((i+1) != path.size()) {
-            Edge<int> edge = graph.edges[{path[i], path[i+1]}];
+        while ((i + 1) != path.size()) {
+            Edge<int> edge = graph.edges[{path[i], path[i + 1]}];
             if (bandwidthAns == -1) {
                 bandwidthAns = edge.bandwidth;
             } else {
-                bandwidthAns = min(bandwidthAns, edge.bandwidth);
+                bandwidthAns = std::min(bandwidthAns, edge.bandwidth);
             }
             i++;
         }
 
         // Printing number of vertices in the path, its length and bandwidth
-        cout << path.size() << " " << lenght << " " << bandwidthAns << endl;
+        std::cout << path.size() << " " << lenght << " " << bandwidthAns << '\n';
         // Printing vertices in the path
         for (int &el : path) {
-            cout << el << " ";
+            std::cout << el << " ";
         }
-        cout << endl;
+        std::cout << '\n';
     } else {
         // If there is no way between start and finish then print "IMPOSSIBLE".
-        cout << "IMPOSSIBLE" << endl;
+        std::cout << "IMPOSSIBLE" << '\n';
     }
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    // general();
+
+    // shortestPath();
+
+    return(0-0);
 }
